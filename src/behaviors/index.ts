79031b9546227eba60e8d05ac631527646e2282d
TheDudeFromCI <thedudefromci@gmail.com>
2020-06-18 20:12:04 -0700

    Added item selection.

--- src/behaviors/index.ts
+++ src/behaviors/index.ts
@@ -1,6 +1,11 @@
+
+export * from "./abstractBehaviorInventory";
+
 export * from "./behaviorFollowEntity";
 export * from "./behaviorIdle";
 export * from "./behaviorLookAtEntity";
 export * from "./behaviorPrintServerStats";
 export * from "./behaviorMoveTo";
 export * from "./behaviorGetClosestEntity";
+export * from "./behaviorSelectItem";
+export * from "./behaviorEquipItem";
