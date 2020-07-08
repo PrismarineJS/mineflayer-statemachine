4c724fc1e0171597d789819bd12ee3c53499ace1
TheDudeFromCI <thedudefromci@gmail.com>
2020-07-08 12:55:08 -0700

    Improved stability of mining.

--- src/behaviors/behaviorMineBlock.ts
+++ src/behaviors/behaviorMineBlock.ts
@@ -1,5 +1,8 @@
 import { StateBehavior, StateMachineTargets } from "../statemachine";
+import { globalSettings } from "../index";
 import { Bot } from "mineflayer";
+import { Item } from "prismarine-item";
+import { Block } from "prismarine-block";
 
 /**
  * This behavior will attempt to break the target block. If the target block
@@ -12,6 +15,11 @@ export class BehaviorMineBlock implements StateBehavior
 
     stateName: string = 'mineBlock';
     active: boolean = false;
+
+    /**
+     * Checks if the bot has finished mining the block or not.
+     */
+    isFinished: boolean = false;
 
     /**
      * Creates a new mine block behavior.
@@ -27,13 +35,52 @@ export class BehaviorMineBlock implements StateBehavior
 
     onStateEntered(): void
     {
+        this.isFinished = false;
+
         if (!this.targets.position)
+        {
+            this.isFinished = true;
             return;
+        }
 
-        let block = this.bot.blockAt(this.targets.position);
-        if (!block || !this.bot.canDigBlock(block) || !this.bot.canSeeBlock(block))
+        const block = this.bot.blockAt(this.targets.position);
+        if (!block || !this.bot.canDigBlock(block))
+        {
+            if (globalSettings.debugMode)
+                console.log("[MineBlock] Cannot mine target block '" + block?.displayName + "'!. Skipping.");
+
+            this.isFinished = true;
             return;
+        }
 
-        this.bot.dig(block);
+        if (globalSettings.debugMode)
+            console.log("[MineBlock] Breaking block '" + block.displayName + "' at " + this.targets.position);
+
+        const tool = this.getBestTool(block);
+        if (tool)
+        {
+            this.bot.equip(tool, 'hand', () =>
+            {
+                this.bot.dig(block, () => this.isFinished = true);
+            });
+        }
+        else
+            this.bot.dig(block, () => this.isFinished = true);
+    }
+
+    private getBestTool(block: Block): Item | undefined
+    {
+        const items = this.bot.inventory.items()
+        for (const i in block.harvestTools)
+        {
+            const id = parseInt(i, 10)
+            for (const j in items)
+            {
+                const item = items[j]
+                if (item.type === id) return item
+            }
+        }
+
+        return undefined;
     }
 }
