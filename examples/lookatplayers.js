36ad2701528e635fcba5ce137b997a9437ab86b0
Javier García <javier.garcia@westwing.es>
2020-11-07 14:54:26 +0100

    npx standard --fix

--- examples/lookatplayers.js
+++ examples/lookatplayers.js
@@ -1,20 +1,19 @@
 /**
  * Set up your bot as you normally would
  */
-const mineflayer = require("mineflayer");
+const mineflayer = require('mineflayer')
 
 if (process.argv.length < 4 || process.argv.length > 6) {
-	console.log('Usage : node lookatplayers.js <host> <port> [<name>] [<password>]')
-	process.exit(1)
+  console.log('Usage : node lookatplayers.js <host> <port> [<name>] [<password>]')
+  process.exit(1)
 }
 
 const bot = mineflayer.createBot({
-	host: process.argv[2],
-	port: parseInt(process.argv[3]),
-	username: process.argv[4] ? process.argv[4] : 'statemachine_bot',
-	password: process.argv[5]
+  host: process.argv[2],
+  port: parseInt(process.argv[3]),
+  username: process.argv[4] ? process.argv[4] : 'statemachine_bot',
+  password: process.argv[5]
 })
-
 
 /**
  * Setting up the state machine is pretty straightforward.
@@ -22,56 +21,55 @@ const bot = mineflayer.createBot({
 
 // Imports
 const {
-	StateTransition,
-	BotStateMachine,
-	BehaviorIdle,
-	BehaviorLookAtEntity,
-	EntityFilters,
-	NestedStateMachine } = require("mineflayer-statemachine");
+  StateTransition,
+  BotStateMachine,
+  BehaviorIdle,
+  BehaviorLookAtEntity,
+  EntityFilters,
+  NestedStateMachine
+} = require('mineflayer-statemachine')
 
 // Wait until we spawn
 bot.on('spawn', () => {
-	// The idle state makes the bot well, idle.
-	const idleState = new BehaviorIdle(bot);
+  // The idle state makes the bot well, idle.
+  const idleState = new BehaviorIdle(bot)
 
-	// This state will allow the bot to look at nearby players.
-	const lookAtPlayersState = new BehaviorLookAtEntity(bot, EntityFilters().PlayersOnly);
+  // This state will allow the bot to look at nearby players.
+  const lookAtPlayersState = new BehaviorLookAtEntity(bot, EntityFilters().PlayersOnly)
 
-	// Now we create a list of all the transitions that can occur between states.
-	const transitions = [
+  // Now we create a list of all the transitions that can occur between states.
+  const transitions = [
 
-		// This transitions from the idleState to the lookAtPlayersState when
-		// someone says hi in chat.
-		new StateTransition({
-			parent: idleState,
-			child: lookAtPlayersState,
-			onTransition: () => bot.chat("hello")
-		}),
+    // This transitions from the idleState to the lookAtPlayersState when
+    // someone says hi in chat.
+    new StateTransition({
+      parent: idleState,
+      child: lookAtPlayersState,
+      onTransition: () => bot.chat('hello')
+    }),
 
-		// This transitions from the lookAtPlayersState to the idleState when
-		// someone says bye in chat. We also want to say bye to the player.
-		new StateTransition({
-			parent: lookAtPlayersState,
-			child: idleState,
-			onTransition: () => bot.chat("goodbye")
-		})
+    // This transitions from the lookAtPlayersState to the idleState when
+    // someone says bye in chat. We also want to say bye to the player.
+    new StateTransition({
+      parent: lookAtPlayersState,
+      child: idleState,
+      onTransition: () => bot.chat('goodbye')
+    })
 
-	];
+  ]
 
-	// Set up some quick events to trigger transitions.
-	bot.on("chat", (username, message) => {
-		if (message === "hi")
-			transitions[0].trigger();
+  // Set up some quick events to trigger transitions.
+  bot.on('chat', (username, message) => {
+    if (message === 'hi') { transitions[0].trigger() }
 
-		if (message === "bye")
-			transitions[1].trigger();
-	});
+    if (message === 'bye') { transitions[1].trigger() }
+  })
 
-	// A state machine is made from a series of layers, so let's create the root
-	// layer to place in our state machine. We just need the transition list and
-	// the starting position.
-	const rootLayer = new NestedStateMachine(transitions, idleState);
+  // A state machine is made from a series of layers, so let's create the root
+  // layer to place in our state machine. We just need the transition list and
+  // the starting position.
+  const rootLayer = new NestedStateMachine(transitions, idleState)
 
-	// Let's add these settings to the state machine and start it!
-	new BotStateMachine(bot, rootLayer);
+  // Let's add these settings to the state machine and start it!
+  new BotStateMachine(bot, rootLayer)
 })
