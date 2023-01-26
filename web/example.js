80da9632b73ba5d6ef66771b4827b3c7f56b7616
Sefirosweb <sefirosweb@gmail.com>
2023-01-26 18:46:21 +0100

    Changes buttons style

--- web/example.js
+++ web/example.js
@@ -102,7 +102,7 @@ tree.json(structure);
 setTimeout(() => {
   tree.forEach((e) => {
     // console.log(e)
-    if (e.node.id === 10)
+    if ([10, 8, 7, 6, 3].includes(e.node.id))
       e.classList.add('active')
   })
 }, 500)
