5f92a55dfbc84e877e61551eed6dbba97d77858b
Sefirosweb <sefirosweb@gmail.com>
2023-01-27 22:26:05 +0100

    Added and fixed bar to move divs

--- web/index.js
+++ web/index.js
@@ -673,3 +673,28 @@ function selectLayer(layer) {
   console.log(`Selected layer ${layer}`)
   currentLayerSelected = layer
 }
+
+
+const BORDER_SIZE = 4;
+const panel = document.getElementById("graph-panel");
+const propertiesPanel = document.getElementById("properties-panel");
+
+function resize(e) {
+  const dx = m_pos - e.x;
+  const ax = e.x - m_pos;
+  m_pos = e.x;
+  console.log((parseInt(getComputedStyle(panel, '').width)))
+  panel.style.width = (parseInt(getComputedStyle(panel, '').width) + dx) + "px";
+  propertiesPanel.style.width = (parseInt(getComputedStyle(propertiesPanel, '').width) + ax) + "px";
+}
+
+panel.addEventListener("mousedown", function (e) {
+  if (e.offsetX < BORDER_SIZE) {
+    m_pos = e.x;
+    document.addEventListener("mousemove", resize, false);
+  }
+}, false);
+
+document.addEventListener("mouseup", function () {
+  document.removeEventListener("mousemove", resize, false);
+}, false);
